3725725b50c1c5008e153529316be739d09af0ac
VinhDang <Vinh.,hardy@gmail.com>
2023-03-03 09:02:10 +0700

    update Script

--- C# Practice/TestingEvents.cs
+++ C# Practice/TestingEvents.cs
@@ -7,70 +7,54 @@ using UnityEngine.Android;
 
 public class TestingEvents : MonoBehaviour
 {
-    // // Define the event / accessor.
-    // // EventHandler is a delegate with two fields.
-    // public event EventHandler<OnSpacePressedEventArgs> OnSpacePressed;
-    // public class OnSpacePressedEventArgs : EventArgs
+    // ----- Part 1 ----
+    // Create an event trigger everytime hit "space" and calculate how many time a "space" been hit in the TestingEvents script.
+    
+    // public event EventHandler<OnSpacePressEventArgs> OnSpacePress;
+    // public class OnSpacePressEventArgs : EventArgs
     // {
     //     public int spaceCount;
     // }
-    //
     // int spaceCount;
-    // void Start()
+    // public void Awake()
     // {
-    //
+    //     OnSpacePress += Testing_OnSpacePress;
     // }
     //
-    // void Update()
+    // public void Update()
     // {
     //     if (Input.GetKeyDown(KeyCode.Space))
     //     {
-    //         //for the trigger
-    //         // OnSpacePressed(this, EventArgs.Empty); <== this will raise an error because it don't have any subscribers.
-    //         // First test the event is not null.
-    //         spaceCount ++;
-    //         OnSpacePressed?.Invoke(this, new OnSpacePressedEventArgs{spaceCount = spaceCount});
+    //         spaceCount++;
+    //         OnSpacePress?.Invoke(this, new OnSpacePressEventArgs {spaceCount = spaceCount});
     //     }
     // }
-    
-    // public static Action Ondelegate = delegate { };
-    //
-    // public void Awake()
+    // void Testing_OnSpacePress(object sender, OnSpacePressEventArgs e)
     // {
-    //     Ondelegate += function;
-    // }
-    //
-    // public void function()
-    // {
-    //     Debug.Log("hello world");
+    //     Debug.Log("TestingEvent: space hit! " + e.spaceCount);
     // }
     
     
-    // Create an event trigger everytime hit "space" and calculate how many time a "space" been hit in the TestingEvents script,
+    // ----- Part 2 -----
+    // Create an event trigger everytime hit "space" and calculate how many time a "space" been hit
+    // but separating logic and visuals.
+    // event and trigger in TestingEvent, function to called in TestingEventSubcriber.
+    // 
 
     public event EventHandler<OnSpacePressEventArgs> OnSpacePress;
     public class OnSpacePressEventArgs : EventArgs
     {
-        public int spaceCount;
+       public int countSpace;
     }
-    int spaceCount;
-    public void Awake()
-    {
-        OnSpacePress += SpacePress;
-    }
+
+    int countSpace;
 
     public void Update()
     {
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            spaceCount++;
-            OnSpacePress?.Invoke(this, new OnSpacePressEventArgs {spaceCount = spaceCount});
+            countSpace++;
+            OnSpacePress?.Invoke(this, new OnSpacePressEventArgs{countSpace = countSpace});
         }
     }
-    void SpacePress(object sender, OnSpacePressEventArgs e)
-    {
-        Debug.Log("space hit! " + e.spaceCount);
-    }
-    
-    
 }
